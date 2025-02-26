034e67d993db151cdc5dcd651c8415a50e28a37a
eversonrpl <everson.8950@aluno.pr.senac.br>
2025-02-26 19:59:32 -0300

    Update temporario.py
    selectbox

--- temporario.py
+++ temporario.py
@@ -3,4 +3,7 @@ st.header("Cabeçalho")
 st.toggle("Toggle")
 st.text_area("enter text")
 st.text_input("movie title", "life of briann")
+st.selectbox(
+  "Qual é a sua cor favorita?",
+  ('azul', 'vermelho', verde"))
 st.button("botão salvar")
