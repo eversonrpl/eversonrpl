1c3829a72d83d868e069015bd054fd380fb6cd04
eversonrpl <everson.8950@aluno.pr.senac.br>
2025-02-26 19:52:19 -0300

    Update temporario.py
    arrumamaos input

--- temporario.py
+++ temporario.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.header("Cabeçalho")
 st.toggle("Toggle")
 st.text_area("enter text")
-st.text.input("movie title", "life of brian")
+st.text_input("movie title", "life of brian")
 st.button("botão salvar")
